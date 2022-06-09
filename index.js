85b9c06a891b04c0128d591a22acdbe51a275439
NicholasKunguKamau <nicholaskungukamau@gmail.com>
2022-06-09 10:25:40 +0300

    commit tree a59370ce6e75f50d58a6bdfca938a2fae7965660

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(){
+    let par1, par2;
+    return (par1 / par2);
+
+}
+ divide(2000, 100);
+
+ let x = 2
+ const square= x ** x
+
+
+function add (){
+    const addi= (para1, para2) => para1 + para2;
+}
+add(3, 4);
